f6d91030c9353d0fed67458cc2cb6add555ed445
Winston Chiong <winstonchiong@berkeley.edu>
2024-05-06 02:02:14 -0700

    rename files, fix kNN bug, add neural network notebook

--- UpdateData.py
+++ UpdateData.py
@@ -1,10 +1,32 @@
-from datacleaning.FetchData import FetchData
-from datacleaning.CleanData import CleanData
+import redis 
+import os
+import pandas as pd 
+from io import BytesIO
+from dotenv import load_dotenv
+
+#! RUN THIS SCRIPT TO UPDATE THE DATA IN THE DATA FOLDER
+
+load_dotenv()
+r = redis.Redis(connection_pool=redis.ConnectionPool.from_url(url=os.getenv("REDIS_URI")))
+
+def get_df(redis_client: redis.Redis, name) -> pd.DataFrame:
+    """
+    Get a dataframe from Redis.
+    """
+    buffer = redis_client.get(name)
+    result = pd.read_parquet(BytesIO(buffer))
+    return result
+
 
 print("Fetching data...")
-raw_data = FetchData.scan_save_all_records()
 
-print("Cleaning data...")
-cleaned_df = CleanData.clean_save_raw_data(raw_data)
+dataframes = ["raw_data", "fivemindemand", "hourlydemand", "dailydemand", "monthlydemand"]
+
+for df in dataframes:
+    print("Fetching data for", df, "...")
+    result = get_df(r, df)
+
+    print("Saving data", df, "...")
+    result.to_csv(f"data/{df}.csv")
 
 print("Done!")
